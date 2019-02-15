e632aee10a4456b7ec9abecd17aced005facd774
Haryz Izzudin <haryzizzudin@gmail.com>
2019-02-15 15:48:01 +0800

    Refactor test data into separate class

--- tests/GuidBase64.IntegrationTests/Base64GuidModelBindingTest.cs
+++ tests/GuidBase64.IntegrationTests/Base64GuidModelBindingTest.cs
@@ -1,3 +1,4 @@
+using GuidBase64.CommonTestData;
 using GuidBase64.TestWebApi;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System;
@@ -17,17 +18,11 @@ namespace GuidBase64.IntegrationTests
             _client = factory.CreateClient();
         }
 
-        public static IEnumerable<object[]> Base64GuidPairData =>
-            new List<object[]>
-            {
-                new object[] { new Guid("00000000-0000-0000-0000-000000000000"), "AAAAAAAAAAAAAAAAAAAAAA" },
-                new object[] { new Guid("c6a44c9f-763a-4524-8c0b-04c599f001a6"), "n0ykxjp2JEWMCwTFmfABpg" },
-                new object[] { new Guid("ffffffff-ffff-ffff-ffff-ffffffffffff"), "_____________________w" }
-            };
+        public static IEnumerable<object[]> Base64GuidPairData => TestData.Base64GuidPairs;
 
         [Theory]
         [MemberData(nameof(Base64GuidPairData))]
-        public async Task CanBindBase64GuidAsync(Guid guid, string encodedBase64Guid)
+        public async Task CanBindBase64Guid(Guid guid, string encodedBase64Guid)
         {
             var httpResponse = await _client.GetAsync($"/api/values/{encodedBase64Guid}");
 
