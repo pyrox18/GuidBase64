eadd2f827d294bccf8ad1fcb17b2e55f7335c4bf
Haryz Izzudin <haryzizzudin@gmail.com>
2019-02-20 18:40:10 +0800

    Add performance tests for ToString method

---
+++ tests/GuidBase64.PerformanceTests/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using GuidBase64.PerformanceTests.Benchmarks;
+
+namespace GuidBase64.PerformanceTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Base64GuidToString>();
+        }
+    }
+}
